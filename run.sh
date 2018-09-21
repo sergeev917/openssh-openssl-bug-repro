d5cddb1ae7029d56480b2955fd4d25fe5ec8f0ed
Alexander Sergeyev <sergeev917@gmail.com>
2018-09-21 16:56:26 +0300

    use docker image id, not tag

--- run.sh
+++ run.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 set -o errexit
 cd "$(dirname "${BASH_SOURCE[0]}")"
-docker build --tag openssh-bug-reproducer .
-docker run -it --rm openssh-bug-reproducer
+
+readonly image_id_path=$(mktemp)
+function cleanup { [[ ! -z "$image_id_path" ]] && rm "$image_id_path"; }
+trap cleanup EXIT
+
+docker build --iidfile="${image_id_path}" .
+echo "=== IMAGE ID: ${image_id_path}: $(< "${image_id_path}") ==="
+docker run -it --rm $(< "${image_id_path}")
